5ddec93411ae6d3c35813d163e47a51112dd7e42
DESKTOP-4D1S1CQ\天然 <aurora314156@gmail.com>
2018-10-18 01:20:30 +0800

    [UPDATE] model how to fit ???

--- Developement/Attention-Bidirection RNN/training.py
+++ Developement/Attention-Bidirection RNN/training.py
@@ -3,7 +3,8 @@ import sys
 import logging
 from gensim.models import Word2Vec
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Embedding
+from keras.layers import Activation, Embedding, Bidirectional, GRU
+
 
 
 def w2vToEmbeddingMatrix(word2vecPath):
@@ -25,19 +26,47 @@ def w2vToEmbeddingMatrix(word2vecPath):
         #    print(j)
     return embeddings_matrix
 
-def w2vToEmbeddingLayer(embeddingDim, embeddingMatrix):
-    
+def test(word2vecPath):
+    # convert the wv word vectors into a numpy matrix that is suitable for insertion
+    # into our TensorFlow and Keras models
+    model = Word2Vec.load(word2vecPath)
+    embedding_matrix = np.zeros((len(model.wv.vocab), vector_dim))
+    for i in range(len(model.wv.vocab)):
+        embedding_vector = model.wv[model.wv.index2word[i]]
+        if embedding_vector is not None:
+            embedding_matrix[i] = embedding_vector
+    valid_size = 16  # Random set of words to evaluate similarity on.
+    valid_window = 100  # Only pick dev samples in the head of the distribution.
+    valid_examples = np.random.choice(valid_window, valid_size, replace=False)
+    # setup the embedding layer
+    embeddings = Embedding(input_dim=embedding_matrix.shape[0], output_dim=embedding_matrix.shape[1],
+                      weights=[embedding_matrix])
+
+def w2vToEmbeddingLayer(embeddingDim, embeddingMatrix, word2vecPath):
+
+    # model.add(Embedding(len(embeddingMatrix),
+    #                         input_length = embeddingDim,
+    #                         output_dim = embeddingDim,
+    #                         weights=[embeddingMatrix],
+    #                         trainable=False))
     print("Start w2vToEmbeddingLayer")
-    embedding_layer = Embedding(len(embeddingMatrix),
-                            input_dim = embeddingDim,
-                            weights=[embeddingMatrix])
-    print("Success")
+    word2vecmodel = Word2Vec.load(word2vecPath)
+    model = Sequential()
+    model.add(word2vecmodel.wv.get_keras_embedding(train_embeddings=False))
+    model.add(Bidirectional(GRU(units = 50)))
+    model.add(Activation('softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy']) 
+    model.summary()
+    #model.fit(embeddingMatrix,embeddingMatrix,epochs=5,batch_size=32)
+    print("train..")
 
 def main():
     
     embeddingDim = 300
-    embeddingMatrix = w2vToEmbeddingMatrix(word2vecPath = sys.argv[1])
-    w2vToEmbeddingLayer(embeddingDim, embeddingMatrix)
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    word2vecPath = sys.argv[1]
+    embeddingMatrix = w2vToEmbeddingMatrix(word2vecPath)
+    w2vToEmbeddingLayer(embeddingDim, embeddingMatrix, word2vecPath)
     
 if __name__ == "__main__":
     main()
