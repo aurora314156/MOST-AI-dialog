7578f73ddbcbbcb4cbc5999a8293ffdee30ae319
aurora314156 <aurora314156@gmail.com>
2018-11-10 18:26:51 +0800

    [UPDATE] GRU network implement done. Next stage need to get the final hidden state from GRU network.

--- Developement/AttentionWithGRU/AttentionWithGRU.py
+++ Developement/AttentionWithGRU/AttentionWithGRU.py
@@ -1,17 +1,11 @@
-import os
-import sys
+import os, sys, logging
 import tensorflow as tf
-import logging
 import numpy as np
-from numpy import array
-from numpy import argmax
-from keras.layers.embeddings import Embedding
-from keras.layers import Input
-from keras.models import Model
-from keras.layers import LSTM
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
-from tensorflow.examples.tutorials.mnist import input_data
+from numpy import array, argmax
+from keras.models import Sequential
+from keras.layers import LSTM, GRU, Dense, RepeatVector, TimeDistributed
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+import matplotlib.pyplot as plt
 sys.path.append('../')
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
@@ -42,50 +36,67 @@ class AttentionWithGRU():
     def bidirectionalGRU(self, questionWordList):
         # forward vector
         fOneHot = self.oneHotEncoding(questionWordList)
+        print(fOneHot.shape)
         self.GRU(fOneHot)
-        
-    def GRU(self, oneHotEncoding):
-        # tf GRU cell
-        batch_size = 4 
-        input = tf.random_normal(shape=[3, batch_size, 6], dtype=tf.float32)
-        cell = tf.nn.rnn_cell.BasicLSTMCell(10, forget_bias=1.0, state_is_tuple=True)
-        init_state = cell.zero_state(batch_size, dtype=tf.float32)
-        output, final_state = tf.nn.dynamic_rnn(cell, input, initial_state=init_state, time_major=True) #time_major如果是True，就表示RNN的steps用第一个维度表示，建议用这个，运行速度快一点。
-        #如果是False，那么输入的第二个维度就是steps。
-        #如果是True，output的维度是[steps, batch_size, depth]，反之就是[batch_size, max_time, depth]。就是和输入是一样的
-        #final_state就是整个LSTM输出的最终的状态，包含c和h。c和h的维度都是[batch_size， n_hidden]
-        with tf.Session() as sess:
-            sess.run(tf.global_variables_initializer())
-            #print(sess.run(output))                                                                                                                                                                                                                                                   
-            #print(sess.run(final_state))
-            print(sess.run([output,final_state]))
+        #print(fOneHot.shape)
+        # backward vector
+        #bOneHot = self.oneHotEncoding(list(reversed(questionWordList)))
+        #backwardVector = self.GRU(bOneHot)
+        #print(bOneHot.shape)
+        # concat vector
+        #concat = tf.concat(0, [fOneHot, bOneHot])
+        #print(concat)
 
-        print("done")
-        
-        
+    def GRU(self, oneHotEncoding):
+        # keras GRU cell
+        # reshape input into [samples, timesteps, features]
+        train_x = oneHotEncoding
+        print(oneHotEncoding.shape)
+        n_in = len(oneHotEncoding)
+        train_x = train_x.reshape((1, n_in, 1))
+        # define model
+        model = Sequential()
+        model.add(GRU(50, activation='relu', input_shape=(n_in,1)))
+        model.add(RepeatVector(n_in))
+        model.add(GRU(50, activation='relu', return_sequences=True))
+        model.add(TimeDistributed(Dense(1, activation='relu')))
+        model.compile(optimizer='adam', loss='mean_squared_error')
+        print(model.summary())
+        history = model.fit(train_x, train_x, epochs = 30)
+        print()
+        # Plot training & validation accuracy values
+        # plt.plot(history.history['acc'])
+        # plt.plot(history.history['val_acc'])
+        # plt.title('Model accuracy')
+        # plt.ylabel('Accuracy')
+        # plt.xlabel('Epoch')
+        # plt.legend(['Train', 'Test'], loc='upper left')
+        # plt.show()
+
+        # Plot training & validation loss values
+        plt.plot(history.history['loss'])
+        #plt.plot(history.history['val_loss'])
+        plt.title('Model loss')
+        plt.ylabel('Loss')
+        plt.xlabel('Epoch')
+        plt.legend(['Train', 'Test'], loc='upper left')
+        plt.show()
+        print()
+
         
     def oneHotEncoding(self, WordList):
         # dict transfer to array
         values = array(WordList)
-        print(values)
         # integer encode
         label_encoder = LabelEncoder()
         integer_encoded = label_encoder.fit_transform(values)
-        print(integer_encoded)
         # binary encode
         onehot_encoder = OneHotEncoder(sparse=False)
-        # create oneHotVector, only need to take maximum number building one-hot vector from array
+        # create one-dim oneHotVector, only need to take maximum number building one-hot vector 
+        # from array then merge all one hot encodeing vector to one-dim vector.
         oneHotV = np.zeros((len(integer_encoded), integer_encoded.max()+1))
-        oneHotV[np.arange(integer_encoded.max()+1), integer_encoded.tolist()] = 1
-        print(oneHotV)
+        oneHotV[np.arange(len(integer_encoded)), integer_encoded] = 1
+        oneHotV = oneHotV.ravel()
+        #print(oneHotV)
 
         return oneHotV
-
-
-    # input1 = Input(shape=(9,9,1), name = "test")
-        # lstm1, state_h, state_c = LSTM(128, return_sequences = True, return_state=True)(input1)
-        # model= Model(inputs = input1, outputs=[lstm1,state_h, state_c])
-        # print("--------------", oneHotEncoding.shape)
-        # model.predict(oneHotEncoding[0])
-        # print(model.summary())
-        # print(lstm1)
