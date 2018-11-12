b1db46f9a775d342e17a149efccba1153949e24b
DESKTOP-T7CIDVH\wirl <aurora314156@gmail.com>
2018-11-12 17:46:44 +0800

    [UPDATE] Question vector representation done. Next step need to implement Attention mechanism.

--- Developement/AttentionWithGRU/AttentionWithGRU.py
+++ Developement/AttentionWithGRU/AttentionWithGRU.py
@@ -2,8 +2,8 @@ import os, sys, logging
 import tensorflow as tf
 import numpy as np
 from numpy import array, argmax
-from keras.models import Sequential
-from keras.layers import LSTM, GRU, Dense, RepeatVector, TimeDistributed
+from keras.models import Sequential, Model
+from keras.layers import LSTM, GRU, Dense, RepeatVector, TimeDistributed, Input
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 import matplotlib.pyplot as plt
 sys.path.append('../')
@@ -20,7 +20,7 @@ class AttentionWithGRU():
         questionWordList = self.CQADataSet[0].getQuestion()
         
         # QuestionBidirectionalGRU
-        self.bidirectionalGRU(questionWordList)
+        questionVector = self.bidirectionalGRU(questionWordList)
 
         # StoryBidirectionalGRU 
         #SotryBidirectionalGRU()
@@ -36,16 +36,19 @@ class AttentionWithGRU():
     def bidirectionalGRU(self, questionWordList):
         # forward vector
         fOneHot = self.oneHotEncoding(questionWordList)
-        print(fOneHot.shape)
-        self.GRU(fOneHot)
-        #print(fOneHot.shape)
+        forwardVector = self.GRUTest(fOneHot)
+        print(forwardVector.shape)
         # backward vector
-        #bOneHot = self.oneHotEncoding(list(reversed(questionWordList)))
-        #backwardVector = self.GRU(bOneHot)
-        #print(bOneHot.shape)
-        # concat vector
-        #concat = tf.concat(0, [fOneHot, bOneHot])
-        #print(concat)
+        bOneHot = self.oneHotEncoding(list(reversed(questionWordList)))
+        backwardVector = self.GRUTest(bOneHot)
+        print(backwardVector.shape)
+        # concat forward vector and backward vector
+        quesitonVector = np.concatenate((forwardVector,backwardVector), axis=None)
+        print(quesitonVector)
+        print(quesitonVector.shape)
+        print(type(quesitonVector))
+
+        return questionVector
 
     def GRU(self, oneHotEncoding):
         # keras GRU cell
@@ -54,25 +57,19 @@ class AttentionWithGRU():
         print(oneHotEncoding.shape)
         n_in = len(oneHotEncoding)
         train_x = train_x.reshape((1, n_in, 1))
+        #parameter
+        n_units = 50
         # define model
         model = Sequential()
-        model.add(GRU(50, activation='relu', input_shape=(n_in,1)))
+        model.add(GRU(n_units, activation='relu', input_shape=(n_in,1)))
         model.add(RepeatVector(n_in))
-        model.add(GRU(50, activation='relu', return_sequences=True))
+        model.add(GRU(n_units, activation='relu', return_sequences=True, return_state=True))
         model.add(TimeDistributed(Dense(1, activation='relu')))
         model.compile(optimizer='adam', loss='mean_squared_error')
         print(model.summary())
         history = model.fit(train_x, train_x, epochs = 30)
         print()
-        # Plot training & validation accuracy values
-        # plt.plot(history.history['acc'])
-        # plt.plot(history.history['val_acc'])
-        # plt.title('Model accuracy')
-        # plt.ylabel('Accuracy')
-        # plt.xlabel('Epoch')
-        # plt.legend(['Train', 'Test'], loc='upper left')
-        # plt.show()
-
+        
         # Plot training & validation loss values
         plt.plot(history.history['loss'])
         #plt.plot(history.history['val_loss'])
@@ -83,7 +80,27 @@ class AttentionWithGRU():
         plt.show()
         print()
 
+    def GRUTest(self, oneHotEncoding):
+        # define timesteps
+        queVlen = len(oneHotEncoding)
+        # define model, save GRU all hidden state and final hidden state for question vector representation
+        inputs = Input(shape=(queVlen,1))
+        temp_all_hidden_state, temp_final_hidden_state = GRU(50, return_sequences=True, return_state=True, activation='softmax')(inputs)
+        model = Model(inputs=inputs, outputs=[temp_all_hidden_state, temp_final_hidden_state])
+        # define input data
+        data = oneHotEncoding.reshape(1,queVlen,1)
+        # train model using encoder method
+        model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
+        mp = model.predict(data, verbose = 1)
+        all_hidden_state, final_hidden_state = mp[0], mp[1]
         
+        #print(all_hidden_state)
+        #print("================================================")
+        #print(final_hidden_state)
+        
+        return final_hidden_state
+        
+
     def oneHotEncoding(self, WordList):
         # dict transfer to array
         values = array(WordList)
@@ -100,3 +117,13 @@ class AttentionWithGRU():
         #print(oneHotV)
 
         return oneHotV
+
+
+# Plot training & validation accuracy values
+        # plt.plot(history.history['acc'])
+        # plt.plot(history.history['val_acc'])
+        # plt.title('Model accuracy')
+        # plt.ylabel('Accuracy')
+        # plt.xlabel('Epoch')
+        # plt.legend(['Train', 'Test'], loc='upper left')
+        # plt.show()
