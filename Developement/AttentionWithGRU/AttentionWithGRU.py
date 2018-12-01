f18222af4e986ff93fd0405f3c83606ec32f7d97
DESKTOP-T7CIDVH\wirl <aurora314156@gmail.com>
2018-12-01 14:31:28 +0800

    [UPDATE] Add free memory

--- Developement/AttentionWithGRU/AttentionWithGRU.py
+++ Developement/AttentionWithGRU/AttentionWithGRU.py
@@ -1,4 +1,4 @@
-import os, sys, logging
+import os, sys, logging, gc
 import tensorflow as tf
 import numpy as np
 import time
@@ -9,7 +9,6 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from numpy import linalg as LA
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import cosine
-import gc
 sys.path.append('../')
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
@@ -22,46 +21,50 @@ class AttentionWithGRU():
         self.tqn = tqn
         
     def AttentionWithGRUMain(self):
-        
         # final answer list
         guessAnsList = []
+        sTime = time.time()
         for i in range(self.tqn):
             print("Processing number: ", i)
             # corpus content initial
             questionWordList = self.CQADataSet[i].getQuestion()
             storyWordList = self.CQADataSet[i].getCorpus()
             answerList = self.CQADataSet[i].getAnswer()
-            sTime = time.time()
+            eachTime = time.time()
             # QuestionBidirectionalGRU input Vector
             forwardV, backwardV = self.OneHotEncoding(questionWordList), self.OneHotEncoding(list(reversed(questionWordList)))
+            #print("forV len:",len(forwardV))
             # StoryBidirectionalGRU 
             storyV = self.BidirectionalStoryGRU(storyWordList)
             #print("storyVector len:", len(storyV))
             # QuestionBidirectionalGRU
             questionV = self.BidirectionalGRU(forwardV, backwardV)
-            #print("questionVector len:", len(questionV))
+            del forwardV, backwardV
             # hops for n iteration
             for h in range(self.hops):
+                print("Start processing hops summed.")
                 # AttentionValue
+                #print("storyVector len:", len(storyV))
                 attentionValueV = self.AttentionValue(storyV, questionV)
                 #print("attentionValueVector length:",len(attentionValueV))
                 # WordLevelAttetion
                 storyWordLevelV = self.WordLevelAttention(storyV, attentionValueV)
-                #print("storyWordLevelV Shape:",storyWordLevelV.shape)
+                del attentionValueV
                 # hops, VQn and VSn+1 summed to form a new question Vector VQn+1
                 for j in range(len(questionV)):
                     storyWordLevelV[j] += questionV[j]
-                
                 # use final attention VS vector as next VQ vector
-                questionV = storyWordLevelV
-                forwardV, backwardV = questionV, np.flip(questionV, axis = 0)
+                forwardV, backwardV = storyWordLevelV, np.flip(storyWordLevelV, axis = 0)
+                del storyWordLevelV, questionV
                 # QuestionBidirectionalGRU
                 questionV = self.BidirectionalGRU(forwardV, backwardV)
+                del forwardV, backwardV
                 print("Finished {} hops summed!".format(h+1))
-                # avoid tensorflow error
+                # free memory
                 gc.collect()
-            
+            del storyV
             # guess answer
+            print("Start calculate answer vector.")
             highestScoreAnswer = 0
             guessAnswer = 1
             ind = 1
@@ -70,23 +73,25 @@ class AttentionWithGRU():
                 ansForwardV, ansBackwardV = self.OneHotEncoding(a), self.OneHotEncoding(list(reversed(a)))
                 # AnswerBidirectionalGRU
                 answerV = self.BidirectionalGRU(ansForwardV, ansBackwardV)
-
+                del ansForwardV, ansBackwardV
                 # use final attention VS vector as FINAL VQ vector
-                forwardV, backwardV = questionV, np.flip(questionV, axis = 0)
-                # FINAL GRU QuestionBidirectionalGRU
-                questionV = self.BidirectionalGRU(forwardV, backwardV)
-
                 # guess answer by calculate cosine value between storyV and answerV
                 tempScoreAnswer = cosine(questionV, answerV)
                 if highestScoreAnswer < tempScoreAnswer:
                     highestScoreAnswer = tempScoreAnswer
                     guessAnswer = ind
-                print("CurrentAnswer score",tempScoreAnswer)
-                print("HighestScoreAnswer score",highestScoreAnswer)
+                #print("CurrentAnswer score",tempScoreAnswer)
+                #print("HighestScoreAnswer score",highestScoreAnswer)
                 ind += 1
-            print("GuessAnswer: ", guessAnswer)
+                # free memory
+                del answerV
+            del questionV
+            gc.collect()
+            #print("GuessAnswer: ", guessAnswer)
             guessAnsList.append(guessAnswer)
-            print("Gru took: %.2fs" % (time.time()-sTime))
+            print("This epoch took: %.2fs" % (time.time()-eachTime))
+            
+        print("Gru took: %.2fs" % (time.time()-sTime))
         return guessAnsList
 
     def WordLevelAttention(self, storyVector, attentionValueVector):
@@ -94,7 +99,7 @@ class AttentionWithGRU():
         storyVector = np.ravel(array(storyVector))
         storyVector = storyVector.tolist()
         wordLevelStoryVector = np.array([(storyVector[i] + storyVector[i+1]) * attentionValueVector[i] for i in range(len(attentionValueVector))])
-        
+        del storyVector
         return wordLevelStoryVector
 
     def AttentionValue(self, storyVector, questionVector):
@@ -110,7 +115,7 @@ class AttentionWithGRU():
         exps = [np.exp(i) for i in attentionValue]
         sum_of_exps = sum(exps)
         attentionValue_softmax = [j/sum_of_exps for j in exps]
-
+        del attentionValue, exps
         return attentionValue_softmax
 
     def BidirectionalGRU(self, forwardV, backwardV):
@@ -124,6 +129,7 @@ class AttentionWithGRU():
         # print(backwardVector.shape)
         quesitonVector = np.concatenate((forwardVector,backwardVector), axis=None)
         # print(quesitonVector.shape)
+        del f_all_hidden_state, f_final_hidden_state, b_all_hidden_state, b_final_hidden_state, forwardVector, backwardVector
         return quesitonVector
 
     def BidirectionalStoryGRU(self, storyWordList):
@@ -140,7 +146,7 @@ class AttentionWithGRU():
         storyVector = []
         for index in range(len(f_all_hidden_state[0])):
            storyVector.append(np.concatenate((f_all_hidden_state[0][index],b_all_hidden_state[0][index]), axis=None))
-        
+        del forwardV, f_all_hidden_state, f_final_hidden_state, backwardV, b_all_hidden_state, b_final_hidden_state
         return storyVector
 
     def GRU(self, inputV):
@@ -151,13 +157,17 @@ class AttentionWithGRU():
         temp_all_hidden_state, temp_final_hidden_state = GRU(self.gru_units, return_sequences=True, return_state=True, activation='softmax')(inputs)
         model = Model(inputs=inputs, outputs=[temp_all_hidden_state, temp_final_hidden_state])
         # define input data
-        data = inputV.reshape(1,seqlen,1)
+        data = inputV.reshape((1,seqlen,1))
         # train model using encoder method
-        model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
+        model.compile(optimizer='adam', loss='mean_squared_error')
+        # train model
+        #model.fit(data, data, epochs = self.model_fit_epochs)
+        # 
         mp = model.predict(data, verbose = 1)
-        
+
         all_hidden_state, final_hidden_state = mp[0], mp[1]
-        
+        del model, mp, inputs, data, temp_all_hidden_state, temp_final_hidden_state
+
         return all_hidden_state, final_hidden_state
 
     def GRUModelEvalute(self, inputV):
@@ -199,6 +209,7 @@ class AttentionWithGRU():
         oneHotV = np.zeros((len(integer_encoded), integer_encoded.max()+1))
         oneHotV[np.arange(len(integer_encoded)), integer_encoded] = 1
         oneHotV = oneHotV.ravel()
+        del values, label_encoder, integer_encoded
         
         return oneHotV
 
